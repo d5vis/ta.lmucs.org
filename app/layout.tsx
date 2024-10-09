998b28f8214e7d95492d2314986517a9bbb1f3c3
d5vis <davisjbanks@gmail.com>
2024-10-09 16:30:39 -0700

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Analytics />
         <meta
           name="theme-color"
           media="(prefers-color-scheme: light)"
