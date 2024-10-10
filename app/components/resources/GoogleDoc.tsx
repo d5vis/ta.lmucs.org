589aa3f0a3e85914c129bb40b8b0ff5ce6b318ae
d5vis <davisjbanks@gmail.com>
2024-10-09 17:00:14 -0700

    add doc iframe component

---
+++ app/components/resources/GoogleDoc.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useEffect, useState } from "react";
+import { Card } from "@/components/ui/card";
+import {
+  generateGoogleDocsEmbedUrl,
+  generateGoogleDocsUrl,
+} from "@/app/utils/googleDocs";
+
+interface GoogleDocProps {
+  documentId: string;
+  title: string;
+}
+
+export default function GoogleDoc(props: GoogleDocProps) {
+  const [isPortrait, setIsPortrait] = useState(false);
+  const [iframeDimensions, setIframeDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsPortrait(window.innerHeight > window.innerWidth);
+      setIframeDimensions({
+        width: window.innerWidth * 0.75,
+        height: window.innerHeight * 0.73,
+      });
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isPortrait) {
+      window.location.replace(generateGoogleDocsUrl(props.documentId));
+    }
+  }, [isPortrait]);
+
+  return (
+    <Card className="w-full h-full flex flex-col items-center justify-center gap-4 rounded-2xl p-8">
+      <iframe
+        className="dark:invert dark:hue-rotate-[170deg] dark:brightness-[0.88] dark:saturate-[0.5] border border-dashed border-border dark:border-gray-300 rounded-xl"
+        src={generateGoogleDocsEmbedUrl(props.documentId)}
+        title={props.title}
+        width={iframeDimensions.width}
+        height={iframeDimensions.height}
+      ></iframe>
+    </Card>
+  );
+}
--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,51 +1,7 @@
-"use client";
-import { useEffect, useState } from "react";
-import { Card } from "@/components/ui/card";
-import {
-  generateGoogleDocsEmbedUrl,
-  generateGoogleDocsUrl,
-} from "../utils/googleDocs";
+import GoogleDoc from "../components/resources/GoogleDoc";
 
 const RESOURCE_DOCUMENT_ID = "1PkUHhsyFGwRSkPc7IIz5VpnajxFUeePNDSOxLczobqk";
 
 export default function Resources() {
-  const [isPortrait, setIsPortrait] = useState(false);
-  const [iframeDimensions, setIframeDimensions] = useState({
-    width: 0,
-    height: 0,
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsPortrait(window.innerHeight > window.innerWidth);
-      setIframeDimensions({
-        width: window.innerWidth * 0.75,
-        height: window.innerHeight * 0.73,
-      });
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (isPortrait) {
-      window.location.replace(generateGoogleDocsUrl(RESOURCE_DOCUMENT_ID));
-    }
-  }, [isPortrait]);
-
-  return (
-    <Card className="w-full h-full flex flex-col items-center justify-center gap-4 rounded-2xl p-8">
-      <iframe
-        className="dark:invert dark:hue-rotate-[170deg] dark:brightness-[0.88] dark:saturate-[0.5]"
-        src={generateGoogleDocsEmbedUrl(RESOURCE_DOCUMENT_ID)}
-        title="Onboarding"
-        width={iframeDimensions.width}
-        height={iframeDimensions.height}
-        style={{ border: "0" }}
-      ></iframe>
-    </Card>
-  );
+  return <GoogleDoc documentId={RESOURCE_DOCUMENT_ID} title="Resources" />;
 }
