5cbf48a9ba6007a224e87d12373768cdb22ed67a
d5vis <davisjbanks@gmail.com>
2025-01-31 15:06:42 -0800

    modify banner hover

--- components/header/Banner.tsx
+++ components/header/Banner.tsx
@@ -1,6 +1,6 @@
 const Banner = ({ body }: { body: React.ReactNode }) => {
   return (
-    <div className="z-40 motion-preset-slide-down motion-delay-200 z-40 w-full bg-gradient-to-r from-lmublue to-lmucrimson text-white p-1 text-sm">
+    <div className="z-40 motion-preset-slide-down motion-delay-200 w-full bg-gradient-to-r from-lmublue via-lmucrimson to-lmublue bg-[size:200%] hover:bg-right text-white p-1 text-sm transition-all duration-500">
       {body}
     </div>
   )
